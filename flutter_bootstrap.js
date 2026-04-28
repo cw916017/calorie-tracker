686cf9a6a85aa1655290d04bd42415c5ccc72ab1
Duke <cw916017@gmail.com>
2026-04-28 23:34:35 +0800

    自律者記錄-底部輸入區正式版

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "747423788" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "713057549" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
